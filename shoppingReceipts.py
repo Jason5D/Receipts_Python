0267a0aea69ea24f85c107bd0c31672a4c321169
Jason <jason5donoghue@gmail.com>
2023-09-13 08:35:30 +0100

    create first variables

---
+++ shoppingReceipts.py
@@ -0,0 +1,19 @@
+lovely_loveseat_description = """
+Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inchs deep. Red or white
+"""
+lovely_loveseat_price = 254.00
+
+stylish_seatee_description = """
+Stylist seatee. Faux leather on birch. 29.50 inches high x 54.75 inches high x 28 inches deep. Black
+"""
+stylish_seatee_price = 180.50
+
+luxurious_lamp_description = """
+Luxurious lamp. Glass and iron. 36 inches tall. Brown with cream shade
+"""
+luxurious_lamp_price = 52.15
+
+sales_tax = 0.88
+
+
+print(lovely_loveseat_description)
